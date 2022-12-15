b9e7ee6bc3f493271245e08406868e4ada479f3c
thsaris <118178034+thsaris@users.noreply.github.com>
2022-12-15 03:04:53 +0200

    tweaks

--- tasks/shop.js
+++ tasks/shop.js
@@ -19,17 +19,19 @@ const prekes = [
     },
 ];
 
-function shop(goods, currency) {
+function shop(prekes, currency) {
     console.log("MUSU PARDUOTUVE:");
     console.log(" ----------------");
     let totalPrice = 0;
-    for (let i = 0; i < prekes.length - 1; i++) {
-        console.log(`${prekes[i].name} kainuoja ${prekes[i].price.toFixed(2)} ${valiuta} ir turime ju ${prekes[i].inStock} vienetu.`);
+    for (let i = 0; i < prekes.length; i++) {
+        console.log(`${prekes[i].name} kainuoja ${prekes[i].price.toFixed(2)} ${currency} ir turime ju ${prekes[i].inStock} vienetu.`);
         totalPrice += prekes[i].price * prekes[i].inStock;
     }
     console.log("----------------");
     console.log(`Viso asortimento kaina: ${totalPrice.toFixed(2)} ${currency}.`);
 }
+
+shop(prekes, currency);
 
 
 //console.log('MUSU PARDUOTUVE:');
